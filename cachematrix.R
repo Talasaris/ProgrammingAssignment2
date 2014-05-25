a765ad5f253c4204df6f36083836aa37790c1c5e
Milos Jovanovic <milos@iskre.net>
2014-05-25 11:38:17 +0200

    Test commit
    Added some comments to test commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions. The first (makeCacheMatrix) creates a matrix object that calculates its inverse
+## and stores the result in the cache.
+## The second one (cacheSolve) either calculates the inverse of the matrix object from makeCacheMatrix or, if 
+## the inverse has already been calculated and the matrix hasnt changed, it retrieves the result from the cache.
 
 ## Write a short comment describing this function
 
